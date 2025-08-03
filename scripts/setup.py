33ff15edd5438885aa4effe9f39d8148463a1e5f
Ryan Rousseau <ryanhrousseau@gmail.com>
2025-08-03 15:58:45 +0100

    added utility functions

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
+import os
+import sys
 
 # Visualization
 import matplotlib.pyplot as plt
@@ -23,11 +25,11 @@ pd.set_option('display.width', 1000)
 plt.style.use('seaborn-v0_8')
 
 # Load pickled datasets
-df = pd.read_pickle('data/processed/cleaned_call_script_data.pkl') # Main dataset, not including February data
-df_volt = pd.read_pickle('data/filtered/data_volt.pkl') 
-df_non_volt = pd.read_pickle('data/filtered/data_non_volt.pkl')
-df_treatment = pd.read_pickle('data/filtered/data_treatment.pkl')
-df_control = pd.read_pickle('data/filtered/data_control.pkl')
+df = pd.read_pickle('../data/processed/cleaned_call_script_data.pkl') # Main dataset, not including February data
+df_volt = pd.read_pickle('../data/filtered/data_volt.pkl') 
+df_non_volt = pd.read_pickle('../data/filtered/data_non_volt.pkl')
+df_treatment = pd.read_pickle('../data/filtered/data_treatment.pkl')
+df_control = pd.read_pickle('../data/filtered/data_control.pkl')
 
 print(f"Main dataset loaded: {df.shape}")
 print(f"VOLT customers: {df_volt.shape[0]}")
